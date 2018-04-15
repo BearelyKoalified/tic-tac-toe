15c452b020c20b9deeea2523bb745886f000c099
Bearely Koalified <BearelyKoalified@gmail.com>
2018-04-15 19:37:17 -0400

    working tic-tac-toe game!

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -22,7 +22,15 @@ class Board extends React.Component {
 
     handleClick(i){
         const squares = this.state.squares.slice();
+        //logic to end game if winner is found
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
+        //switches x and o turns
         squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+        //sets board value to x or o and who's next value
         this.setState({
             squares: squares,
             xIsNext: !this.state.xIsNext, //flip X/O flag on click
@@ -39,7 +47,13 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        }else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -86,3 +100,23 @@ ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
